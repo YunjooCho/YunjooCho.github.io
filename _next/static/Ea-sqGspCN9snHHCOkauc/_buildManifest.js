36abb508d85313d25404025f691d4168fcff213b
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2022-10-20 06:58:25 +0000

    Deploying to gh-pages from @ YunjooCho/YunjooCho.github.io@3186dcdc8dad204204854c016778e3d890aaa646 🚀

--- _next/static/Ea-sqGspCN9snHHCOkauc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7afdf1c7554f3922.js"],"/Feature":["static/chunks/pages/Feature-c2c4dbe3bf1819b2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/Feature","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
