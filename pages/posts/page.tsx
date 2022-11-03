6eb805331779ef9b30616303ce4381ab8954f19a
YunjooCho <rollcake0317@naver.com>
2022-11-03 12:15:38 +0900

    2022/11/03 retry update

--- pages/posts/page.tsx
+++ pages/posts/page.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Author from '../../components/_child/Author'
 import Format from '../../layout/format'
+import Image from 'next/image'
 
 const Page: NextPage = () => {
     return (
@@ -8,6 +9,14 @@ const Page: NextPage = () => {
             <section className="container mx-auto md:px-2 py-16 w-1/2">
                 <div className="flex justify-center">
                     <Author />
+                </div>
+                <div className="post py-10">
+                    <h1 className="font-bold text-4xl text-center pb-5">블로그 만들기</h1>
+                    <p className="text-gray-500 text-xl text-center">유튜브 인강으로 배웠습니다. <br/>재미있습니다.</p>
+                    <div className="py-10">
+                        <Image src={"/images/blog.avif"} width={900} height={600}></Image>
+                        <div className="content text-gray-600 text-lg flex flex-col gat-4"></div>
+                    </div>
                 </div>
             </section>
         </Format>
